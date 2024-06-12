69bec139900c4df8fbd0054ef4d898862ca1d2b4
SsandySANTHOSH <santhoshs4717@gmail.com>
2024-06-12 13:42:59 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f8b4aa664msh041f52601e82a24p1cfdffjsn67cb9fca4176",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6a3ce1a555faf0ee322ac3ca0926e8e2";
+
